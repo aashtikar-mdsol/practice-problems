10863efb1b23bc782ed92e0a78a36ae0c670acc8
aneeshashtikar <aneeshashtikargithub@gmail.com>
2024-07-10 04:32:55 +0530

    base commit

--- src/com/QuestionTwo.java
+++ src/com/QuestionTwo.java
@@ -2,28 +2,22 @@ package com;
 
 public class QuestionTwo {
 
-    public static int solution(String S) {
-        int N = S.length();
-        int[] prefix_A = new int[N + 1];
-        int[] suffix_B = new int[N + 1];
+    public static int solution(String input) {
+        var strLen = input.length();
+        var prefixB = new int[strLen + 1];
+        var suffixA = new int[strLen + 1];
 
-        // Compute prefix_A
-        for (int i = 0; i < N; i++) {
-            prefix_A[i + 1] = prefix_A[i] + (S.charAt(i) == 'A' ? 1 : 0);
+        for (int i = 0; i < strLen; i++) {
+            prefixB[i + 1] = prefixB[i] + (input.charAt(i) == 'B' ? 1 : 0);
         }
-
-        // Compute suffix_B
-        for (int i = N - 1; i >= 0; i--) {
-            suffix_B[i] = suffix_B[i + 1] + (S.charAt(i) == 'B' ? 1 : 0);
+        for (int i = strLen - 1; i >= 0; i--) {
+            suffixA[i] = suffixA[i + 1] + (input.charAt(i) == 'A' ? 1 : 0);
         }
-
-        // Calculate the minimum deletions
         int minDeletions = Integer.MAX_VALUE;
-        for (int i = 0; i <= N; i++) {
-            int deletions = prefix_A[i] + suffix_B[i];
+        for (int i = 0; i <= strLen; i++) {
+            int deletions = prefixB[i] + suffixA[i];
             minDeletions = Math.min(minDeletions, deletions);
         }
-
         return minDeletions;
     }
 
